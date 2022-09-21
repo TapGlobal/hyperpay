d1744504193a27508c1bcf695382a4b9c9183786
Chinedu Ohagwu <chinedu@youverify.co>
2022-09-22 00:55:57 +0100

    changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,12 @@ export default {
       },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon-32x32.png' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon-32x32.png' },
+      { rel: "preconnect", href: "https://fonts.googleapis.com" },
+      { rel: "preconnect",  href:"https://fonts.gstatic.com" },
+      { href: "https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300&display=swap", rel:"stylesheet"}
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
